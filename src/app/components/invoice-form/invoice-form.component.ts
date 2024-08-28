3a15246b3bc8d0d364c2d9826499311435993d94
Silas Owu <owusilasyp00@gmail.com>
2024-08-28 07:11:54 -0700

    ✨ Feat: add functionality to add items to invoice form

--- src/app/components/invoice-form/invoice-form.component.ts
+++ src/app/components/invoice-form/invoice-form.component.ts
@@ -1,22 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { FormButtonComponent } from "../reusables/form-button/form-button.component";
 import { ModalService } from '../../services/modal.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-invoice-form',
   standalone: true,
-  imports: [FormButtonComponent],
+  imports: [FormButtonComponent,FormsModule],
   templateUrl: './invoice-form.component.html',
   styleUrl: './invoice-form.component.css'
 })
 export class InvoiceFormComponent {
-
- constructor(private modalService: ModalService){}
-
   @Input() editMode:boolean = false;
   @Input() editInvoiceData: any;
   showTerms:boolean = false;
   paymentTerm:string = "Select payment terms";
+  itemList:Array<{name:string , qty: number, price: number}>= [];
+
+ constructor(private modalService: ModalService){}
+
 
   showTermsOption(){
     this.showTerms =!this.showTerms;
@@ -24,7 +26,17 @@ export class InvoiceFormComponent {
   selectTerms(term: any){
     this.paymentTerm = term
   }
+
+  // must go into invoice-form-ops
   discardTerms(){
     this.modalService.hideCreateModal()
+  }
+
+  addListItem():void{
+    this.itemList.push({ name: '', qty: 0, price: 0 });
+  }
+  
+  removeListItem(index: number): void{
+    this.itemList.splice(index, 1);
   }
 }
